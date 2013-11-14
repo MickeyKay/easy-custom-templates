e6dfda652a6a0391d41c479170b97d1393636d25
Mickey Kay <mickey@mickeykaycreative.com>
2013-11-13 19:09:26 -0800

    First commit

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   Easy Custom Templates
+ * @author    MIGHTYminnow <mickey@mightyminnow.com>
+ * @license   GPL-2.0+
+ * @link      http://mightyminnow.com
+ * @copyright 2013 Your Name or Company Name
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// TODO: Define uninstall functionality here
